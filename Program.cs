a8ec3e77809d6028e7737bc397dc76f3507d18ec
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-18 22:36:09 +0400

    Setup commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StackScribe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my StackScribe application");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No command line arguments");
+            }
+            else
+            {
+                foreach(var arg in args)
+                {
+                    Console.WriteLine($"Argument: {arg}");
+                }
+            }
+        }
+    }
+}
