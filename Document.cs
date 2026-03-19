b6149297bc5a4910d3f09dbc94b1425e3148ff25
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-19 12:13:43 +0400

    Commit 1: Created Document.cs with private lines list to store text; AddLine() method to insert text at a position; ShowAll() method to display with line numbers

---
+++ Document.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackScribe
+{
+    public class Document
+    {
+        private List<string> lines = new List<string>();
+
+        public void Addline(int lineNumber, string text)
+        {
+            lines.Insert(lineNumber-1, text);
+        }
+
+        public void ShowAll()
+        {
+            if (lines.Count == 0)
+            {
+                Console.WriteLine("Document is empty");
+            }
+            else
+            {
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    Console.WriteLine($"{i+1}: {lines[i]}");
+                }
+            }
+        }
+    }
+}
