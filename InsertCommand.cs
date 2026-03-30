a9002b0507164e557de51a4bd6bd01916da321f6
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-30 22:18:05 +0400

    Commit 11: Add InsertCommand class for command pattern implementation
    Created InsertCommand.cs with Execute() and Undo() methods to enable undo functionality. This class stores insert operation details (document, line number, text) and can execute or reverse the operation on demand. Foundation for stack-based undo system.

---
+++ InsertCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StackScribe
+{
+    public class InsertCommand
+    {
+        private Document _document;
+        private int _lineNumber;
+        private string _text;
+        public InsertCommand(Document document, int lineNumber, string text)
+        {
+            _document = document;
+            _lineNumber = lineNumber;
+            _text = text;
+        }
+        public void Execute()
+        {
+            _document.Addline(_lineNumber, _text);
+        }
+        public void Undo()
+        {
+            _document.Removeline(_lineNumber);
+        }
+    }
+}
